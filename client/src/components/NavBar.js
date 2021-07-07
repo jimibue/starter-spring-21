0a853e3743b5d315c22b2d6b551f85227c4c0f0d
James Yeates <jimibue@users.noreply.github.com>
2021-07-07 11:04:25 -0600

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,42 +2,6 @@ import React from 'react'
 import { Link, useLocation, withRouter } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
-// const NavBar = (props) => {
-//     const { pathname } = useLocation()
-//     return(
-
-//         <Menu>
-//             <Link to='/'>
-//               <Menu.Item active={pathname == '/'} >
-//                 Home
-//               </Menu.Item >
-//             </Link>
-//             <Link to='/about'>
-//               <Menu.Item active={pathname == '/about'} >
-//                 About    
-//              </Menu.Item >
-//             </Link>
-//         </Menu>
-//     )
-// }
-
-// const NavBar = (props) => {
-//     const { location } = props
-//     return(
-//         <Menu>
-//             <Link to='/'>
-//               <Menu.Item active={location.pathname == '/'} >
-//                 Home
-//               </Menu.Item >
-//             </Link>
-//             <Link to='/about'>
-//               <Menu.Item active={location.pathname == '/about'} >
-//                 About    
-//              </Menu.Item >
-//             </Link>
-//         </Menu>
-//     )
-// }
 
 class NavBar extends React.Component {
     render(){
@@ -50,26 +14,6 @@ class NavBar extends React.Component {
                   <Menu.Item active={location.pathname == '/'} >
                     Home
                   </Menu.Item >
-                </Link>
-                <Link to='/users'>
-                  <Menu.Item active={location.pathname == '/users'} >
-                    Users    
-                 </Menu.Item >
-                </Link>
-                <Link to='/grades'>
-                  <Menu.Item active={location.pathname == '/grades'} >
-                    Grades    
-                 </Menu.Item >
-                </Link>
-                <Link to='/skills'>
-                  <Menu.Item active={location.pathname == '/skills'} >
-                    Skills    
-                 </Menu.Item >
-                </Link>
-                <Link to='/about'>
-                  <Menu.Item active={location.pathname == '/about'} >
-                    About    
-                 </Menu.Item >
                 </Link>
                 <Link to='/componentDemo'>
                   <Menu.Item active={location.pathname == '/componentDemo'} >
